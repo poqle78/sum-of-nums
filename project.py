bb3ceac62948fd2ddab851977dc209459fe02823
poqle78 <karamov.07@bk.ru>
2025-09-19 23:18:51 +0300

    Add files via upload
    небольшие измения для красоты

--- project.py
+++ project.py
@@ -4,7 +4,7 @@ def f(trg, nums):
     flag2 = 1
     if len(nums) > 1:
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
+            for j in range(i + 1, len(nums)):
                 if i not in checked and j not in checked:
                     if nums[i] + nums[j] == trg:
                         if flag1:
@@ -12,20 +12,21 @@ def f(trg, nums):
                             checked.append(i)
                             checked.append(j)
                             flag1 = 0
-                            print('Others:', end = ' ')
+                            print('Others:', end=' ')
                             flag2 = 0
                         else:
-                            print(f'({i}, {j})', end = ' ')
+                            print(f'({i}, {j})', end=' ')
                             checked.append(i)
                             checked.append(j)
         if flag2:
             print('None')
     else:
         print('The list must contain more than 1 element!')
+
+
 try:
-    nums = [int(i) for i in input().split()]
-    trg = int(input())
+    nums = [int(i) for i in input('Введите список (через пробел): ').split()]
+    trg = int(input("Введите искомую сумму: "))
     f(trg, nums)
 except ValueError:
     print('That was no valid number')
-
